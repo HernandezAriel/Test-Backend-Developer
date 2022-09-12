c339f36a3bdb55cdf2f9e91b05e513cfdc67461c
Claudio <cynad97@gmail.com>
2022-09-12 11:07:15 -0300

    CandidateByTechnology find methods

--- test/src/main/java/com/testbackend/test/repositories/CandidateByTechnologyRepository.java
+++ test/src/main/java/com/testbackend/test/repositories/CandidateByTechnologyRepository.java
@@ -1,11 +1,18 @@
 package com.testbackend.test.repositories;
 
+import com.testbackend.test.models.entities.Candidate;
 import com.testbackend.test.models.entities.CandidateByTechnology;
+import com.testbackend.test.models.entities.Technology;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface CandidateByTechnologyRepository extends JpaRepository<CandidateByTechnology, Long>{
 
+    CandidateByTechnology findByCandidateAndTechnology(Candidate candidate, Technology technology);
+    List<CandidateByTechnology> findByCandidate(Candidate candidate);
+    List<CandidateByTechnology> findByTechnology(Technology technology);
 }
