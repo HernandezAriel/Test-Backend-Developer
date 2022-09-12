aaa3fcf5e2c3d238ac46b990ad23055e47ac87ef
Claudio <cynad97@gmail.com>
2022-09-12 12:16:45 -0300

    Creating Controllers

--- test/src/main/java/com/testbackend/test/controllers/CandidateController.java
+++ test/src/main/java/com/testbackend/test/controllers/CandidateController.java
@@ -1,14 +1,19 @@
 package com.testbackend.test.controllers;
 
 import com.testbackend.test.models.dtos.CandidateDto;
+import com.testbackend.test.models.entities.Candidate;
 import com.testbackend.test.services.Imp.CandidateService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +35,23 @@ public class CandidateController {
     @GetMapping("/{idCandidate}")
     public ResponseEntity<CandidateDto> getCandidateById(@PathVariable Long idCandidate){
         return ResponseEntity.ok(candidateService.getCandidateDtoById(idCandidate));
+    }
+
+    @PostMapping
+    public ResponseEntity addCandidate(@RequestBody Candidate candidate){
+        candidateService.addCandidate(candidate);
+        return ResponseEntity.ok().body("Success. ");
+    }
+
+    @PutMapping("/{idCandidate}")
+    public ResponseEntity<Candidate> updateCandidate(@RequestBody Candidate candidate) {
+        return ResponseEntity.ok().body(candidateService.addCandidate(candidate));
+    }
+
+    @DeleteMapping("/{idCandidate}")
+    public ResponseEntity<String> deleteCandidate(@PathVariable Long idCandidate){
+        candidateService.deleteCandidate(idCandidate);
+        return ResponseEntity.ok().body("Usuario eliminado");
     }
 
 }
