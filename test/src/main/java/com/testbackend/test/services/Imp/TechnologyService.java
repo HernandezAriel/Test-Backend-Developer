ea7e5c454a58a5845413b92c8d0f5c7db163806e
Claudio <cynad97@gmail.com>
2022-09-12 11:54:38 -0300

    Technology Service Working

--- test/src/main/java/com/testbackend/test/services/Imp/TechnologyService.java
+++ test/src/main/java/com/testbackend/test/services/Imp/TechnologyService.java
@@ -6,6 +6,7 @@ import com.testbackend.test.repositories.TechnologyRepository;
 import com.testbackend.test.services.ITechnologyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +18,11 @@ public class TechnologyService implements ITechnologyService {
     private TechnologyRepository technologyRepository;
     @Autowired
     private CandidateByTechnologyService candidateByTechnologyService;
+    private final ModelMapper modelMapper;
+
+    public TechnologyService(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
 
     public Technology addTechnology(Technology technology){
         return technologyRepository.save(technology);
@@ -24,7 +30,9 @@ public class TechnologyService implements ITechnologyService {
 
     public List<TechnologyDto> getAllTechnologies(){
         List<TechnologyDto> technologiesDto = new ArrayList<>();
-        technologiesDto = technologyRepository.findAll();
+        for(Technology technology: technologyRepository.findAll()){
+            technologiesDto.add(modelMapper.map(technology,TechnologyDto.class));
+        }
         return technologiesDto;
     }
 
@@ -32,9 +40,9 @@ public class TechnologyService implements ITechnologyService {
         return technologyRepository.findById(idTechnology).orElse(null);
     }
 
-    public TechnologyDto getTechnologyDto(Long idTechnology){
+    public TechnologyDto getTechnologyDtoById(Long idTechnology){
         Technology technology = technologyRepository.findById(idTechnology).orElse(null);
-        return technology;
+        return modelMapper.map(technology, TechnologyDto.class);
     }
 
     public Technology updateTechnology(Technology technology){
@@ -43,7 +51,7 @@ public class TechnologyService implements ITechnologyService {
 
     public void deleteTechnology(Long idTechnology){
         Technology technology = getTechnologyById(idTechnology);
-        if(isNull(candidateByTechnologyService.getCandidatesByTechnologyByTechnology(technology)))
+        if((candidateByTechnologyService.getCandidatesByTechnologyByTechnology(technology))==null)
             technologyRepository.deleteById(idTechnology);
     }
 }
