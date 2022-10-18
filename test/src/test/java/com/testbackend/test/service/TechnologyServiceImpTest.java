9860ee409a42c52934a19b2835c72912ee5c9d62
Claudio <cynad97@gmail.com>
2022-10-18 13:44:50 -0300

    Create addTechnologyOkTest

---
+++ test/src/test/java/com/testbackend/test/service/TechnologyServiceImpTest.java
@@ -0,0 +1,43 @@
+package com.testbackend.test.service;
+
+import com.testbackend.test.exception.TechnologyAlreadyExistsException;
+import com.testbackend.test.model.entity.Technology;
+import com.testbackend.test.repository.TechnologyRepository;
+import com.testbackend.test.service.imp.CandidateByTechnologyServiceImp;
+import com.testbackend.test.service.imp.TechnologyServiceImp;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.modelmapper.ModelMapper;
+import static org.mockito.Mockito.mock;
+
+import static com.testbackend.test.testUtil.TechnologyTestUtil.getTechnology;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class TechnologyServiceImpTest {
+
+    private TechnologyRepository technologyRepository;
+    private CandidateByTechnologyServiceImp candidateByTechnologyServiceImp;
+    private ModelMapper modelMapper;
+    private TechnologyServiceImp technologyServiceImp;
+
+    @Before
+    public void start() {
+        technologyRepository = mock(TechnologyRepository.class);
+        candidateByTechnologyServiceImp = mock(CandidateByTechnologyServiceImp.class);
+        modelMapper = mock(ModelMapper.class);
+        technologyServiceImp = new TechnologyServiceImp(technologyRepository, candidateByTechnologyServiceImp, modelMapper);
+    }
+
+    @Test
+    public void addTechnologyOkTest() throws TechnologyAlreadyExistsException {
+        when(technologyRepository.findByNameAndVersion("Java", "11")).thenReturn(null);
+        when(technologyRepository.save(getTechnology())).thenReturn(getTechnology());
+        Technology technology = technologyServiceImp.addTechnology(getTechnology());
+        Assertions.assertEquals(getTechnology(), technology);
+        verify(technologyRepository, times(1)).findByNameAndVersion("Java", "11");
+        verify(technologyRepository, times(1)).save(getTechnology());
+    }
+}
