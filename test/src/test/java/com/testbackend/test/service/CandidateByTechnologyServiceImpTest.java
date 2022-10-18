0dcab9e8eeaf3125047dc573c88110c16cb4852f
Claudio <cynad97@gmail.com>
2022-10-18 14:36:48 -0300

    Create getCandidatesForTechnologyByCandidateOkTest

--- test/src/test/java/com/testbackend/test/service/CandidateByTechnologyServiceImpTest.java
+++ test/src/test/java/com/testbackend/test/service/CandidateByTechnologyServiceImpTest.java
@@ -1,17 +1,22 @@
 package com.testbackend.test.service;
 
 import com.testbackend.test.exception.CandidateByTechnologyAlreadyExistsException;
+import com.testbackend.test.model.entity.CandidateByTechnology;
 import com.testbackend.test.repository.CandidateByTechnologyRepository;
 import com.testbackend.test.service.imp.CandidateByTechnologyServiceImp;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static com.testbackend.test.testUtil.CandidateTestUtil.getCandidate;
 import static com.testbackend.test.testUtil.TechnologyTestUtil.getTechnology;
 import static com.testbackend.test.testUtil.CandidateByTechnologyTestUtil.getCandidateByTechnology;
+import static com.testbackend.test.testUtil.CandidateByTechnologyTestUtil.getListCandidateByTechnology;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -44,4 +49,14 @@ public class CandidateByTechnologyServiceImpTest {
         verify(candidateByTechnologyRepository, times(1)).findByCandidateAndTechnology(getCandidate(), getTechnology());
         verify(candidateByTechnologyRepository, times(0)).save(getCandidateByTechnology());
     }
+
+    @Test
+    public void getCandidatesForTechnologyByCandidateOkTest() {
+        when(candidateByTechnologyRepository.findByCandidate(getCandidate())).thenReturn(getListCandidateByTechnology());
+        List<CandidateByTechnology> cxt = candidateByTechnologyServiceImp.getCandidatesByTechnologyByCandidate(getCandidate());
+        Assertions.assertEquals(getListCandidateByTechnology().size(), cxt.size());
+        Assertions.assertEquals(getListCandidateByTechnology().get(0), cxt.get(0));
+        verify(candidateByTechnologyRepository, times(1)).findByCandidate(getCandidate());
+    }
+
 }
