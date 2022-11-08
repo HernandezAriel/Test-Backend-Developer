35a9d5b1564759146722fe7469ce055a8f738b18
Claudio <cynad97@gmail.com>
2022-11-08 09:57:37 -0300

    Add updateCandidate

--- src/main/java/com/testbackend/test/service/imp/CandidateServiceImp.java
+++ src/main/java/com/testbackend/test/service/imp/CandidateServiceImp.java
@@ -59,7 +59,8 @@ public class CandidateServiceImp implements CandidateService {
     }
 
     public Candidate getCandidateById(Long idCandidate) throws CandidateNotExistsException {
-        return candidateRepository.findById(idCandidate).orElseThrow(() -> new CandidateNotExistsException("Candidate Not Exists"));
+        return candidateRepository.findById(idCandidate)
+                .orElseThrow(() -> new CandidateNotExistsException("Candidate Not Exists"));
     }
 
     public CandidateDto getCandidateDtoById(Long idCandidate) throws CandidateNotExistsException {
@@ -68,36 +69,23 @@ public class CandidateServiceImp implements CandidateService {
         return modelMapper.map(candidate, CandidateDto.class);
     }
 
-    public Candidate addTechnologyToCandidate(Long idCandidate, Long idTechnology, Long experience) throws CandidateNotExistsException, TechnologyNotExistsException, CandidateByTechnologyAlreadyExistsException {
-        Candidate candidate = getCandidateById(idCandidate);
-        Technology technology = technologyServiceImp.getTechnologyById(idTechnology);
-        candidateByTechnologyServiceImp.addCandidateByTechnology(candidate, technology, experience);
-        return candidate;
-    }
 
-    public Set<CandidateDto> getCandidatesByTechnology(String nameTechnology) {
-        Set<CandidateDto> candidatesDto = new HashSet<>();
-        List<CandidateByTechnology> candidatesForTechnologies = candidateByTechnologyServiceImp.getCandidatesByTechnologyByNameTechnology(nameTechnology);
+//    public Set<CandidateDto> getCandidatesByTechnology(String nameTechnology) {
+//        Set<CandidateDto> candidatesDto = new HashSet<>();
+//        List<CandidateByTechnology> candidatesForTechnologies = candidateByTechnologyServiceImp.getCandidatesByTechnologyByNameTechnology(nameTechnology);
+//
+//        for (CandidateByTechnology cbt : candidatesForTechnologies) {
+//            List<ExperienceDto> technologies = new ArrayList<>();
+//            for (ExperienceDto experience : candidateByTechnologyServiceImp.getExperiencesByCandidate(cbt.getCandidate())) {
+//                if (experience.getName().equals(nameTechnology)) technologies.add(experience);
+//            }
+//            candidatesDto.add(converterCandidateToDtoExp(cbt.getCandidate(), technologies));
+//        }
+//        return candidatesDto;
+//    }
 
-        for (CandidateByTechnology cbt : candidatesForTechnologies) {
-            List<ExperienceDto> technologies = new ArrayList<>();
-            for (ExperienceDto experience : candidateByTechnologyServiceImp.getExperiencesByCandidate(cbt.getCandidate())) {
-                if (experience.getName().equals(nameTechnology)) technologies.add(experience);
-            }
-            candidatesDto.add(converterCandidateToDtoExp(cbt.getCandidate(), technologies));
-        }
-        return candidatesDto;
-    }
-
-    public Candidate updateCandidate(Candidate candidate) throws CandidateNotExistsException {
-        if (candidate.getIdCandidate() == null || getCandidateById(candidate.getIdCandidate()) == null) {
-            log.error("Candidate not exists");
-            throw new CandidateNotExistsException("Candidate not exists");
-        } else {
-            log.info("Candidate updated");
-            return candidateRepository.save(candidate);
-        }
-
+    public void updateCandidate(CandidateDto candidateDto, Long id) {
+        candidateRepository.save(modelMapper.map(candidateDto, getCandidateById(id).getClass()));
     }
 
     public void deleteCandidate(Long idCandidate) throws CandidateNotExistsException {
@@ -109,6 +97,13 @@ public class CandidateServiceImp implements CandidateService {
             log.info("Candidate deleted");
             candidateRepository.deleteById(idCandidate);
         }
+    }
+
+    public Candidate addTechnologyToCandidate(Long idCandidate, Long idTechnology, Long experience) throws CandidateNotExistsException, TechnologyNotExistsException, CandidateByTechnologyAlreadyExistsException {
+        Candidate candidate = getCandidateById(idCandidate);
+        Technology technology = technologyServiceImp.getTechnologyById(idTechnology);
+        candidateByTechnologyServiceImp.addCandidateByTechnology(candidate, technology, experience);
+        return candidate;
     }
 }
 
