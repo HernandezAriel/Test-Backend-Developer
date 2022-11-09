b61d4ac668e4ca6cb6e893896fae4501b0f71eca
Claudio <cynad97@gmail.com>
2022-11-09 07:56:59 -0300

    Delete addCandidatesToTechnology

--- src/main/java/com/testbackend/test/service/CandidateService.java
+++ src/main/java/com/testbackend/test/service/CandidateService.java
@@ -2,16 +2,21 @@ package com.testbackend.test.service;
 
 import com.testbackend.test.model.dto.CandidateDto;
 import com.testbackend.test.model.entity.Candidate;
+
 import java.util.List;
 
 public interface CandidateService {
 
     CandidateDto addCandidate(CandidateDto candidateDto);
+
     List<CandidateDto> getAllCandidates();
+
     Candidate getCandidateById(Long idCandidate);
+
     CandidateDto getCandidateDtoById(Long idCandidate);
+
     void updateCandidate(CandidateDto candidateDto, Long id);
+
     void deleteCandidate(Long idCandidate);
-    Candidate addCandidateByTechnology(Long idCandidate, Long idTechnology, Long experience);
 
 }
